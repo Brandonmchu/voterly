4268e2646013b7076d63db49e06987ef03930aaa
Brandonmchu <brandon.mk.chu@gmail.com>
2014-04-18 16:09:26 -0400

    initial commit, has submission model and controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Voterly::Application.config.secret_key_base = '7add37d29e4bc8d71da25b37eb4ea5492ad0e26dd20a02f4b9b18bc26a215771f6b6ae3f314d38ee1559784eb789434de4a8564a8cfe72ba82758bd151afdbcf'
---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Submission.create!(income: 100000, ans_one: 1, ans_two: 2, ans_three: 8)
+Submission.create!(income: 50000, ans_one: 1, ans_two: 1, ans_three: 1)
+Submission.create!(income: 125000, ans_one: 2, ans_two: 9, ans_three: 3)
